25d61218d99adeecf41353a216e44d5152960cbf
you21979 <you2197901@gmail.com>
2013-02-04 21:07:19 +0900

    redisモジュールの追加し忘れ

---
+++ io_redis.ts
@@ -0,0 +1,61 @@
+///<reference path='typescript/node.d.ts'/>
+var redis = require('redis');
+export interface Callback{
+    chMessage(redis:Factory, channel:string, message:string):void;
+}
+export interface Config{
+    hostname:string;
+    port:Number;
+    channels:string[];
+}
+export class Factory{
+    private conn_:any = null;
+    private hostname_:string = null;
+    private port_:Number = 0;
+    /**
+     *
+     */
+    constructor(){}
+    /**
+     *
+     */
+    public connect(config:Config, cb:Callback):void{
+        var self:Factory = this;
+        this.conn_ = new redis.createClient(config.port, config.hostname);
+        config.channels.forEach((channel:string)=>{
+            self.conn_.subscribe(channel);
+        });
+        this._setEvent(cb);
+        this.port_ = config.port;
+        this.hostname_ = config.hostname;
+    }
+    /**
+     *
+     */
+    public disconnect():void{
+        if(this.conn_){
+        }
+    }
+    /**
+     *
+     */
+    public send(channel:string, message:string):void{
+        var conn = redis.createClient(this.port_, this.hostname_);
+        conn.publish(channel, message);
+    }
+    /**
+     *
+     */
+    private _setEvent(cb:Callback):void{
+        var self:Factory = this;
+        this.conn_.on("error", (err:string):void=> {
+            console.log("Error " + err);
+        });
+        this.conn_.on("message", (channel:string, message:string):void=> {
+            console.log(channel + " :" + message);
+            if(cb.chMessage){
+                cb.chMessage(self, channel, message);
+            }
+        })
+    }
+}
